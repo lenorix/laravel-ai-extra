854eb53d895bdbf0c915e2ac967b89fab3a48d45
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-21 13:43:11 +0200

    Add ChatInSession trait for managing chat session storage

---
+++ src/Traits/ChatInSession.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lenorix\LaravelAiExtra\Traits;
+
+trait ChatInSession
+{
+    protected function getSessionKey(): string
+    {
+        return 'laravel-ai-chat-' . str_replace('\\', '-', static::class);
+    }
+
+    public function loadChat(): void
+    {
+        $this->messages = session($this->getSessionKey(), []);
+    }
+
+    public function saveChat(): void
+    {
+        session([$this->getSessionKey() => $this->messages]);
+    }
+}
