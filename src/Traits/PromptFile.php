1dc61a782a28cc058cea26f56ae41b6165653e79
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-21 16:52:04 +0200

    Add PromptFile trait to load prompt files from resources/prompts directory

---
+++ src/Traits/PromptFile.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaravelAiExtra\Traits;
+
+use Illuminate\Support\Facades\File;
+
+trait PromptFile
+{
+    /**
+     * Load a prompt file from the resources/prompts directory.
+     *
+     * @param string $name
+     * @return string
+     */
+    public function loadPrompt(string $name): string
+    {
+        return File::get(resource_path("prompts/{$name}.txt"));
+    }
+}
