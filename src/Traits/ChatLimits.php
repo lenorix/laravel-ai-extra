6623ee75bad9f189c6952d20a85a610ea81cb005
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-24 01:03:35 +0200

    Refactor ChatLimits trait to improve message size handling and ensure limits are enforced

--- src/Traits/ChatLimits.php
+++ src/Traits/ChatLimits.php
@@ -2,6 +2,9 @@
 
 namespace Lenorix\LaravelAiExtra\Traits;
 
+use MalteKuhr\LaravelGPT\Enums\ChatRole;
+use MalteKuhr\LaravelGPT\Models\ChatMessage;
+
 /*
  * Methods to control context limits for maltekuhr/laravel-gpt `GPTChat`.
  *
@@ -10,8 +13,13 @@ namespace Lenorix\LaravelAiExtra\Traits;
 trait ChatLimits
 {
     /**
-     * The maximum number of messages allowed in the chat.
+     * The maximum size of a message in bytes.
      */
+    protected int $maxMessageSize = 5_000;
+    /**
+     * The maximum size of all messages in bytes.
+     */
+    protected int $maxTotalSize = 110_000;
     protected int $maxMessages = 200;
 
     protected function ensureMessagesLimit(): void
@@ -19,5 +27,14 @@ trait ChatLimits
         if (count($this->messages) > $this->maxMessages) {
             $this->messages = array_slice($this->messages, -$this->maxMessages);
         }
+        $this->messages = array_filter($this->messages, function (ChatMessage $message) {
+            return $this->isMessageUnderLimit($message);
+        });
+    }
+
+    protected function isMessageUnderLimit(ChatMessage $message): bool
+    {
+        $encodedMessage = json_encode($message);
+        return $encodedMessage !== false && strlen($encodedMessage) <= $this->maxMessageSize;
     }
 }
