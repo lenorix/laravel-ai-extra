355bca5e05b5a2fdad2aaf594f61c0ff29afee24
jhg <1288711+jhg@users.noreply.github.com>
2025-04-25 02:54:22 +0000

    Fix styling

--- src/Traits/ChatMarkdown.php
+++ src/Traits/ChatMarkdown.php
@@ -2,9 +2,8 @@
 
 namespace Lenorix\LaravelAiExtra\Traits;
 
-use MalteKuhr\LaravelGPT\Enums\ChatRole;
-use MalteKuhr\LaravelGPT\Models\ChatMessage;
 use League\CommonMark\CommonMarkConverter;
+use MalteKuhr\LaravelGPT\Models\ChatMessage;
 
 trait ChatMarkdown
 {
@@ -18,9 +17,11 @@ trait ChatMarkdown
      */
     public function chatMessagesHtml(): array
     {
-        $converter = new CommonMarkConverter();
+        $converter = new CommonMarkConverter;
+
         return array_map(function (ChatMessage $message) use ($converter) {
             $content = $converter->convert($message->content)->getContent();
+
             return new ChatMessage($message->role, content: $content);
         }, $this->chatMessages());
     }
