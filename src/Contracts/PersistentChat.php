35023727b9917265a3452d24ebd37ec76daf8a5c
Jesus Hernandez <jesushdez@protonmail.com>
2025-05-15 12:30:57 +0200

    Improve PersistentChat contract

--- src/Contracts/PersistentChat.php
+++ src/Contracts/PersistentChat.php
@@ -2,9 +2,36 @@
 
 namespace Lenorix\LaravelAiExtra\Contracts;
 
+use MalteKuhr\LaravelGPT\Models\ChatMessage;
+
+/**
+ * Interface for persistent chat functionality.
+ *
+ * This must be followed by traits like ChatSession and ChatDatabase
+ * even PHP can not check it actually.
+ */
 interface PersistentChat
 {
-    public function loadChat(): void;
+    /**
+     * Load chat messages from a persistent storage.
+     *
+     * @param int|null $maxLatestMessages Maximum number of latest messages to load.
+     * @return static
+     */
+    public function loadChat(?int $maxLatestMessages=null): static;
 
-    public function saveChat(): void;
+    /**
+     * Add a message to the chat, storing it or queuing it to ve stored when calling `saveChat()`.
+     *
+     * @param ChatMessage|string $message The message to add.
+     * @return static
+     */
+    public function addMessage(ChatMessage|string $message): static;
+
+    /**
+     * Save the chat messages or do nothing if `addMessage()` stored them.
+     *
+     * @return static
+     */
+    public function saveChat(): static;
 }
--- src/Traits/ChatSession.php
+++ src/Traits/ChatSession.php
@@ -18,9 +18,13 @@ trait ChatSession
         return 'laravel-ai-chat-'.str_replace('\\', '-', static::class);
     }
 
-    public function loadChat(): void
+    public function loadChat(?int $maxLatestMessages=null): void
     {
         $this->messages = session($this->getSessionKey(), []);
+
+        if ($maxLatestMessages) {
+            $this->messages = array_slice($this->messages, -$maxLatestMessages);
+        }
     }
 
     public function saveChat(): void
